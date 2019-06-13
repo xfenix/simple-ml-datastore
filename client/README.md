8693cd69ce91bf281a9c38ddbd1c30767a18217f
Denis Anikin <ad@xfenix.ru>
2019-06-14 00:33:40 +0300

    Simple cache for client library, and also readme

---
+++ client/README.md
@@ -0,0 +1,5 @@
+# Client lib
+### Usage example
+* Provide `ML_DATASTORE_ENDPOINT` environment variable (via `export ML_DATASTORE_ENDPOINT=...`)
+* `from ml_datastor_fetcher import get_word_vector`
+* Use it `get_word_vector('королева')`
--- client/ml_datastore_fetcher.py
+++ client/ml_datastore_fetcher.py
@@ -5,14 +5,19 @@ from gensim.models import Word2Vec
 
 
 ENV_ENDPOINT_NAME = 'ML_DATASTORE_ENDPOINT'
-API_ENDPOINT = os.getenv(ENV_ENDPOINT_NAME, None)
+API_ENDPOINT = os.getenv(ENV_ENDPOINT_NAME, 'http://127.0.0.1:8000/')
 if API_ENDPOINT is None:
     raise EnvironmentError(
         'There is no API endpoint path. '
         'Please, provide {} environment variable'.format(ENV_ENDPOINT_NAME))
 API_ENDPOINT = API_ENDPOINT.rstrip('/')
+LOCAL_CACHE = {}
 
 
 def get_word_vector(word: str) -> Word2Vec:
-    vector_raw_data = requests.get('{}/{}'.format(API_ENDPOINT, word))
+    if word in LOCAL_CACHE:
+        vector_raw_data = LOCAL_CACHE[word]
+    else:
+        vector_raw_data = requests.get('{}/{}'.format(API_ENDPOINT, word))
+        LOCAL_CACHE[word] = vector_raw_data
     return Word2Vec(vector_raw_data)
